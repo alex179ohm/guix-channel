0233466829fd72146030b4c2a101b7c777c030a3
alex179ohm <alex179ohm@gmail.com>
2022-04-02 18:03:34 +0200

    Add ohm packages.scm file

---
+++ ohm/packages.scm
@@ -0,0 +1,32 @@
+(define-module (ohm packages)
+  #:use-module (guix packages)
+  #:use-module (gnu packages crates-io)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system cargo)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix utils))
+
+(define-pubblic rust-aodv
+    (package
+        (name "rust-aodv")
+        (version "0.1")
+        (build-system cargo-build-system)
+        (synopsis "aodv:  Ad Hoc Distance Vector based link Protocol")
+        (description "")
+        (home-page "https://github.com/alex179ohm/aodv")
+        (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                        (url "https://github.com/alex179ohm/aodv.git")
+                        ;;(commit "a606d63")
+                        (commit "f21ccc6")
+                        ;;(commit "main")
+                        ))
+                (sha256
+                (base32
+                "0gbp6imky8p8l1sq7arzgly680ryz251cbakdckic296jhidbmkk"))))
+        (arguments
+        `(#:cargo-inputs
+        (("rust-bytes" ,rust-bytes-1)
+            ("rust-socket2" ,rust-socket2-0.4))))
+    (license (list license:expat license:asl2.0))))
